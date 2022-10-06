9eeb008e8d75b0af252b95fec02e0ef99ab2ee8c
Victor <victorio.deg@gmail.com>
2022-10-05 21:02:47 -0300

    ajustes

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views  # Importa a própria pasta 'blog'.
+from . import views  # Importa o arquivo views.py da própria pasta (.) que o urls.py se encontra.
 
+# Crio uma list urlpatterns = [] igual a do arquivo na pasta mãe.
 urlpatterns = [
     path('', views.index)  # Não executo o método com (), só referencio
+    # Está em '' pois referencia ao próprio url /blog/.
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,3 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-
-def index(request):  # Nome do método poderia ser qualquer um. Index é convenção. O nome request também é convenção. Toda vida é feito um request ao abrir a página.
-    return HttpResponse("Olá mundo!")
